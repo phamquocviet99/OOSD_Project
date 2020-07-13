1d4e016e958f712424c61f52c95f4d823b6e1b77
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-07-13 13:47:18 +0700

    update Company and Product

--- Final_Project/DAO/CompanyDAO.cs
+++ Final_Project/DAO/CompanyDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Final_Project.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,58 @@ namespace Final_Project.DAO
         {
             get { if (instance == null) instance = new CompanyDAO(); return CompanyDAO.instance; }
             private set { CompanyDAO.instance = value; }
+        }
+
+        public List<Company> GetListCompany()
+        {
+            List<Company> list = new List<Company>();
+            string query = " Select * from Company";
+            DataTable data = MyDB.Instance.ExcuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                Company company = new Company(item);
+                list.Add(company);
+            }
+            return list;
+        }
+
+        public bool InsertCompany(string name, string address, string phone)
+        {
+
+            string query = string.Format("INSERT dbo.Company ( namecom , address, phone)VALUES  ( N'{0}', N'{1}', N'{2}')", name, address, phone);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+
+        public bool UpdateCompany(string name, int id, string address, string phone)
+        {
+            string query = string.Format("UPDATE dbo.Company SET namecom = N'{0}', address = N'{2}', phone = N'{3}' WHERE idcate = {1}", name, id, address, phone);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public bool DeleteCompany(int id)
+        {
+            string query = string.Format("Delete dbo.Company where IDC = {0}", id);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public bool DeleteCompanyByID(int id)
+        {
+            string query = string.Format("Delete dbo.Company where IDC = {0}", id);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public Company GetCompanyByID(int id)
+        {
+            Company company = null;
+            string query = "SELECT*FROM Company where IDC=" + id;
+            DataTable data = MyDB.Instance.ExcuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                company = new Company(item);
+                return company;
+            }
+            return company;
         }
     }
 }
---
+++ Final_Project/DAO/ProductDAO.cs
@@ -0,0 +1,74 @@
+﻿using Final_Project.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project.DAO
+{
+    class ProductDAO
+    {
+        private static ProductDAO instance;
+        public static ProductDAO Instance
+        {
+            get { if (instance == null) instance = new ProductDAO(); return ProductDAO.instance; }
+            private set { ProductDAO.instance = value; }
+        }
+
+        public List<Product> GetListProduct()
+        {
+            List<Product> list = new List<Product>();
+            string query = " Select * from Product";
+            DataTable data = MyDB.Instance.ExcuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                Product product = new Product(item);
+                list.Add(product);
+            }
+            return list;
+        }
+
+        public bool InsertProduct(string name, float price, int discount, int warranty, string origin)
+        {
+
+            string query = string.Format("INSERT dbo.Product ( namepro , pricepro, discountpro, warrantypro, originpro)VALUES  " +
+                                         "( N'{0}', N'{1}', N'{2}', N'{3}', N'{4}')", name, price, discount, warranty, origin);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+
+        public bool UpdateProduct(string name, int id, float price, int discount, int warranty, string origin)
+        {
+            string query = string.Format("UPDATE dbo.Product SET namepro = N'{0}', pricepro = N'{2}', discountpro = N'{3}', " +
+                                         " discountpro = N'{4}', originpro = N'{5}' WHERE idpro = {1}", name, id, price, discount, warranty, origin);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public bool DeleteProduct(int id)
+        {
+            string query = string.Format("Delete dbo.Product where IDPro = {0}", id);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public bool DeleteProductByID(int id)
+        {
+            string query = string.Format("Delete dbo.Product where IDPro = {0}", id);
+            int result = MyDB.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public Product GetProductByID(int id)
+        {
+            Product product = null;
+            string query = "SELECT*FROM Product where IDPro =" + id;
+            DataTable data = MyDB.Instance.ExcuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                product = new Product(item);
+                return product;
+            }
+            return product;
+        }
+    }
+}
