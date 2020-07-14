40995f61fc21486de2a1fde73c882ed1a2e03a45
Trung Phan <kaitrung99@gmail.com>
2020-07-14 13:51:45 +0700

    update frontend login

--- Final_Project/GUI/User/Login_Form.Designer.cs
+++ Final_Project/GUI/User/Login_Form.Designer.cs
@@ -28,142 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbpassword = new System.Windows.Forms.TextBox();
-            this.tbusername = new System.Windows.Forms.TextBox();
-            this.btncancel = new System.Windows.Forms.Button();
-            this.btnlogin = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pic_close = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // tbpassword
-            // 
-            this.tbpassword.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.tbpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tbpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.tbpassword.Location = new System.Drawing.Point(12, 224);
-            this.tbpassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tbpassword.Name = "tbpassword";
-            this.tbpassword.Size = new System.Drawing.Size(310, 23);
-            this.tbpassword.TabIndex = 5;
-            // 
-            // tbusername
-            // 
-            this.tbusername.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.tbusername.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tbusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.tbusername.Location = new System.Drawing.Point(12, 158);
-            this.tbusername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tbusername.Name = "tbusername";
-            this.tbusername.Size = new System.Drawing.Size(310, 23);
-            this.tbusername.TabIndex = 4;
-            // 
-            // btncancel
-            // 
-            this.btncancel.BackColor = System.Drawing.Color.Silver;
-            this.btncancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btncancel.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
-            this.btncancel.ForeColor = System.Drawing.Color.White;
-            this.btncancel.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.btncancel.Location = new System.Drawing.Point(11, 329);
-            this.btncancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btncancel.Name = "btncancel";
-            this.btncancel.Size = new System.Drawing.Size(310, 46);
-            this.btncancel.TabIndex = 2;
-            this.btncancel.Text = "Thoát";
-            this.btncancel.UseVisualStyleBackColor = false;
-            // 
-            // btnlogin
-            // 
-            this.btnlogin.BackColor = System.Drawing.Color.Silver;
-            this.btnlogin.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
-            this.btnlogin.ForeColor = System.Drawing.Color.White;
-            this.btnlogin.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.btnlogin.Location = new System.Drawing.Point(11, 270);
-            this.btnlogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(310, 46);
-            this.btnlogin.TabIndex = 1;
-            this.btnlogin.Text = "Đăng nhập";
-            this.btnlogin.UseVisualStyleBackColor = false;
-            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.tbpassword);
-            this.panel1.Controls.Add(this.tbusername);
-            this.panel1.Controls.Add(this.btncancel);
-            this.panel1.Controls.Add(this.btnlogin);
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(327, 383);
-            this.panel1.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
-            this.label2.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.label2.Location = new System.Drawing.Point(18, 193);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 17);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "PASSWORD";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
-            this.label1.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.label1.Location = new System.Drawing.Point(18, 131);
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(551, 79);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 17);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "USERNAME";
+            this.label1.Size = new System.Drawing.Size(281, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login Form";
             // 
-            // label3
+            // textBox1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F);
-            this.label3.ForeColor = System.Drawing.Color.LightBlue;
-            this.label3.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.label3.Location = new System.Drawing.Point(64, 34);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(178, 56);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Cuong";
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(65, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(458, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(433, 53);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(458, 165);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(433, 40);
+            this.panel1.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::Final_Project.Properties.Resources._lock;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(7, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(38, 30);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pic_close
+            // 
+            this.pic_close.BackgroundImage = global::Final_Project.Properties.Resources._106830;
+            this.pic_close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_close.Image = global::Final_Project.Properties.Resources._106830;
+            this.pic_close.Location = new System.Drawing.Point(963, 12);
+            this.pic_close.Name = "pic_close";
+            this.pic_close.Size = new System.Drawing.Size(28, 29);
+            this.pic_close.TabIndex = 5;
+            this.pic_close.TabStop = false;
+            this.pic_close.Click += new System.EventHandler(this.pic_close_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Final_Project.Properties.Resources.login;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(347, 405);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Location = new System.Drawing.Point(458, 223);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(433, 40);
+            this.panel2.TabIndex = 8;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::Final_Project.Properties.Resources.key;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(7, 5);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(38, 30);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(65, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(363, 33);
+            this.textBox2.TabIndex = 2;
             // 
             // Login_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 389);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1003, 404);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pic_close);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login_Form";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbpassword;
-        private System.Windows.Forms.TextBox tbusername;
-        private System.Windows.Forms.Button btncancel;
-        private System.Windows.Forms.Button btnlogin;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pic_close;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- Final_Project/GUI/User/Login_Form.cs
+++ Final_Project/GUI/User/Login_Form.cs
@@ -19,23 +19,9 @@ namespace Final_Project.GUI.User
             InitializeComponent();
         }
 
-        private void btnlogin_Click(object sender, EventArgs e)
+        private void pic_close_Click(object sender, EventArgs e)
         {
-            string userName = tbusername.Text;
-            string passWord = tbpassword.Text;
-
-            if (AccountDAO.Instance.Login(userName, passWord))
-            {
-                //Account loginAccount = AccountDAO.Instance.GetAccountByUserName(userName);
-                Admin_Form admind_form = new Admin_Form();
-                admind_form.ShowDialog();
-                this.Show();
-
-            }
-            else
-            {
-                MessageBox.Show("Sai tên hoặc mật khẩu !");
-            }
+            this.Close();
         }
     }
 }
